5e47ea46886390bcecfdf1044b9ef4cf6d7c2a73
Simon MacMullen <simon@rabbitmq.com>
2011-09-19 12:30:19 +0100

    Abstraction.

--- src/sockjs_filters.erl
+++ src/sockjs_filters.erl
@@ -21,72 +21,73 @@ filters() ->
 
 %% --------------------------------------------------------------------------
 
+%% This is send but it receives - "send" from the client POV, receive
+%% from ours.
 xhr_send(Req, _Server, SessionId, Receive) ->
     receive_body(Req:get(body), SessionId, Receive),
     Req:respond(204).
+
+xhr_polling(Req, _Server, SessionId, _Receive) ->
+    headers(Req),
+    reply_loop(Req, SessionId, true, fun fmt_xhr/1).
+
+%% TODO Do something sensible with client closing timeouts
+xhr_streaming(Req, _Server, SessionId, _Receive) ->
+    headers(Req),
+    %% IE requires 2KB prefix:
+    %% http://blogs.msdn.com/b/ieinternals/archive/2010/04/06/comet-streaming-in-internet-explorer-with-xmlhttprequest-and-xdomainrequest.aspx
+    chunk(Req, list_to_binary(string:copies("h", 2048)), fun fmt_xhr/1),
+    reply_loop(Req, SessionId, false, fun fmt_xhr/1).
+
+jsonp_send(Req, _Server, SessionId, Receive) ->
+    Body = proplists:get_value("d", Req:parse_post()),
+    receive_body(Body, SessionId, Receive),
+    Req:respond(200, "").
+
+jsonp(Req, _Server, SessionId, _Receive) ->
+    headers(Req),
+    CB = list_to_binary(proplists:get_value("c", Req:parse_qs())),
+    reply_loop(Req, SessionId, true, fun (Body) -> fmt_jsonp(Body, CB) end).
+
+%% --------------------------------------------------------------------------
 
 receive_body(Body, SessionId, Receive) ->
     Decoded = mochijson2:decode(Body),
     Sender = sockjs_session:sender(SessionId),
     [Receive(Sender, {recv, Msg}) || Msg <- Decoded].
 
-xhr_polling(Req, Server, SessionId, Receive) ->
+headers(Req) ->
+    headers(Req, "application/javascript; charset=UTF-8").
+
+headers(Req, ContentType) ->
+    Req:chunk(head, [{"Content-Type", ContentType}]).
+
+reply_loop(Req, SessionId, Once, Fmt) ->
     case sockjs_session:reply(SessionId) of
         wait  -> receive
-                     go -> xhr_polling(Req, Server, SessionId, Receive)
+                     go -> reply_loop(Req, SessionId, Once, Fmt)
                  after 5000 ->
-                         reply(Req, <<"h">>)
+                         chunk(Req, <<"h">>, Fmt),
+                         reply_loop0(Req, SessionId, Once, Fmt)
                  end;
-        Reply -> reply(Req, Reply)
+        Reply -> chunk(Req, Reply, Fmt),
+                 reply_loop0(Req, SessionId, Once, Fmt)
     end.
 
-reply(Req, Body) ->
-    Req:ok([], <<Body/binary, $\n>>).
+reply_loop0(Req, _SessionId, true, _Fmt) ->
+    io:format("end!~n"),
+    Req:chunk(done);
+reply_loop0(Req, SessionId, false, Fmt) ->
+    reply_loop(Req, SessionId, false, Fmt).
 
-%% TODO commonality with xhr_polling?
-%% TODO Detect client closing connection sanely - ATM we get an =ERROR
-%% REPORT==== a few seconds after the client goes away
-xhr_streaming(Req, Server, SessionId, Receive) ->
-    Req:chunk(head,
-              [{"Content-Type", "application/javascript; charset=UTF-8"}]),
-    %% IE requires 2KB prefix:
-    %% http://blogs.msdn.com/b/ieinternals/archive/2010/04/06/comet-streaming-in-internet-explorer-with-xmlhttprequest-and-xdomainrequest.aspx
-    chunk(Req, list_to_binary(string:copies("h", 2048))),
-    xhr_streaming0(Req, Server, SessionId, Receive).
+chunk(Req, Body, Fmt) ->
+    Req:chunk(Fmt(Body)).
 
-xhr_streaming0(Req, Server, SessionId, Receive) ->
-    case sockjs_session:reply(SessionId) of
-        wait  -> receive
-                     go -> ok
-                 after 5000 ->
-                         chunk(Req, <<"h">>)
-                 end;
-        Reply -> chunk(Req, Reply)
-    end,
-    xhr_streaming0(Req, Server, SessionId, Receive).
+fmt_xhr(Body) -> <<Body/binary, $\n>>.
 
-chunk(Req, Body) ->
-    Req:chunk(<<Body/binary, $\n>>).
-
-jsonp_send(Req, _Server, SessionId, Receive) ->
-    receive_body(proplists:get_value("d", Req:parse_post()),
-                 SessionId, Receive),
-    Req:respond(200, "").
-
-jsonp(Req, Server, SessionId, Receive) ->
-    Callback = list_to_binary(proplists:get_value("c", Req:parse_qs())),
-    case sockjs_session:reply(SessionId) of
-        wait  -> receive
-                     go -> jsonp(Req, Server, SessionId, Receive)
-                 after 5000 ->
-                         jsonp_reply(Req, Callback, <<"h">>)
-                 end;
-        Reply -> jsonp_reply(Req, Callback, Reply)
-    end.
-
-jsonp_reply(Req, Callback, Body) ->
+fmt_jsonp(Body, Callback) ->
     %% Yes, JSONed twice, there isn't a a better way, we must pass
     %% a string back, and the script, will be evaled() by the
     %% browser.
     Double = iolist_to_binary(mochijson2:encode(Body)),
-    Req:ok([], <<Callback/binary, "(", Double/binary, ");", $\r, $\n>>).
+    <<Callback/binary, "(", Double/binary, ");", $\r, $\n>>.
