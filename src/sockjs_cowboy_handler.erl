9d6c520f07aad7220eeca5a22f0359ee5f26e0e7
Simon MacMullen <simon@rabbitmq.com>
2011-10-04 17:57:20 +0100

    ...and websockets support for cowboy.

--- src/sockjs_cowboy_handler.erl
+++ src/sockjs_cowboy_handler.erl
@@ -1,9 +1,20 @@
 -module(sockjs_cowboy_handler).
 -behaviour(cowboy_http_handler).
+-behaviour(cowboy_http_websocket_handler).
+
 -export([init/3, handle/2, terminate/2]).
+-export([websocket_init/3, websocket_handle/3,
+         websocket_info/3, websocket_terminate/3]).
+
+-define(WS_MODULE, sockjs_ws).
+-record(state, {self, recv}).
 
 init({tcp, http}, Req, _Opts) ->
-    {ok, Req, undefined_state}.
+    {Upgrade, Req1} = cowboy_http_req:header('Upgrade', Req),
+    case Upgrade of
+        <<"WebSocket">> -> {upgrade, protocol, cowboy_http_websocket};
+        _               -> {ok,      Req1,     undefined_state}
+    end.
 
 handle(Req, State) ->
     {Path0, Req1} = cowboy_http_req:raw_path(Req),
@@ -19,6 +30,44 @@ handle(Req, State) ->
     {ok, Req2, State}.
 
 terminate(_Req, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------------
+
+websocket_init(_TransportName, Req, _Opts) ->
+    {Path0, Req1} = cowboy_http_req:raw_path(Req),
+    Path = clean_path(binary_to_list(Path0)),
+    {Receive, _, _, _} = sockjs_filters:dispatch('GET', Path,
+                                                 sockjs_test:dispatcher()),
+    Self = {?WS_MODULE, self()},
+    self() ! {send, ["o"]},
+    Receive(Self, init),
+    {ok, Req1, #state{self = Self, recv = Receive}}.
+
+websocket_handle({text, Text}, Req,
+                 State = #state{self = Self, recv = Receive}) ->
+    Decoded = sockjs_util:decode(Text),
+    Receive(Self, {recv, Decoded}),
+    {ok, Req, State};
+
+websocket_handle(Data, Req, State) ->
+    io:format("Handle ~p~n", [Data]),
+    {ok, Req, State}.
+
+websocket_info({send, Msg}, Req, State) ->
+    {reply, {text, Msg}, Req, State};
+%% Client closed
+websocket_info(closed, Req, State = #state{self = Self, recv = Receive}) ->
+    Receive(Self, closed),
+    {shutdown, Req, State};
+%% Server closed
+websocket_info(shutdown, Req, State) ->
+    {shutdown, Req, State};
+websocket_info(Info, Req, State = #state{self = Self, recv = Receive}) ->
+    Receive(Self, {info, Info}),
+    {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
     ok.
 
 %% --------------------------------------------------------------------------
@@ -45,12 +94,6 @@ config_js(Req) ->
                    200, [{<<"content-type">>, <<"application/javascript">>}],
                    "var client_opts = {\"url\":\"http://localhost:8080\",\"disabled_transports\":[],\"sockjs_opts\":{\"devel\":true}};", Req),
     Req1.
-
-ws_loop(Ws) ->
-    Path = clean_path(Ws:get(path)),
-    %%io:format("~s ~s~n", ["WS", Path]),
-    {Fun, _, _, _} = sockjs_filters:dispatch('GET', Path, sockjs_test:dispatcher()),
-    sockjs_ws:loop(Ws, Fun).
 
 clean_path("/")         -> "index.html";
 clean_path("/" ++ Path) -> Path.
--- src/sockjs_ws.erl
+++ src/sockjs_ws.erl
@@ -3,36 +3,13 @@
 -behaviour(sockjs_sender).
 
 -export([send/2, close/3]).
--export([loop/2]).
 
 %% TODO this has little in common with the other transports
 %% Where should framing happen? (Do we care?)
 
 send(Data, {?MODULE, Ws}) ->
-    Ws:send(["m", sockjs_util:encode(Data)]).
+    Ws ! {send, ["m", sockjs_util:encode(Data)]}.
 
 close(Code, Reason, {?MODULE, Ws}) ->
-    Ws:send(["c", sockjs_util:encode([Code, list_to_binary(Reason)])]),
-    exit(normal). %% TODO ?
-
-%% --------------------------------------------------------------------------
-
-loop(Ws, Receive) ->
-    Ws:send(["o"]),
-    Self = {?MODULE, Ws},
-    Receive(Self, init),
-    loop0(Ws, Receive, Self).
-
-loop0(Ws, Receive, Self) ->
-    receive
-        {browser, Data} ->
-            Decoded = sockjs_util:decode(Data),
-            Receive(Self, {recv, Decoded}),
-            loop0(Ws, Receive, Self);
-        closed ->
-            Receive(Self, closed),
-            closed;
-        Msg ->
-            Receive(Self, {info, Msg}),
-            loop0(Ws, Receive, Self)
-    end.
+    Ws ! {send, ["c", sockjs_util:encode([Code, list_to_binary(Reason)])]},
+    Ws ! shutdown.
