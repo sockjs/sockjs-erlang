0c70e1ce635bf23ed89044b79f447d5cc0cf4ac4
Marek Majkowski <majek04@gmail.com>
2012-02-14 15:04:42 +0000

    catch nosession error when got some data in ws

--- src/sockjs_ws_handler.erl
+++ src/sockjs_ws_handler.erl
@@ -13,17 +13,22 @@ received(_RawWebsocket, _SessionPid, <<>>) ->
 received(websocket, SessionPid, Data) ->
     case sockjs_json:decode(Data) of
         {ok, Msg} when is_binary(Msg) ->
-            sockjs_session:received([Msg], SessionPid),
-            ok;
+            session_received([Msg], SessionPid);
         {ok, Messages} when is_list(Messages) ->
-            sockjs_session:received(Messages, SessionPid),
-            ok;
+            session_received(Messages, SessionPid);
         _Else ->
             shutdown
     end;
 
 received(rawwebsocket, SessionPid, Data) ->
-    sockjs_session:received([Data], SessionPid).
+    session_received([Data], SessionPid).
+
+session_received(Messages, SessionPid) ->
+    try sockjs_session:received(Messages, SessionPid) of
+        ok         -> ok
+    catch
+        no_session -> shutdown
+    end.
 
 -spec reply(websocket|rawwebsocket, pid()) -> {close|open, binary()} | wait.
 reply(websocket, SessionPid) ->
