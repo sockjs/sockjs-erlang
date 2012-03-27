970be4927217fcd9d92299a5c70c1708a0ae2fc0
Isaev Ivan <1@kraslan.ru>
2012-03-28 00:43:27 +0800

    fix {odd_info,heartbeat_triggered}

--- src/sockjs_session.erl
+++ src/sockjs_session.erl
@@ -285,6 +285,8 @@ handle_info(heartbeat_triggered, State = #session{response_pid = RPid}) when RPi
     RPid ! go,
     {noreply, State#session{heartbeat_tref = triggered}};
 
+handle_info(heartbeat_triggered, State) -> {stop, normal, State};
+
 handle_info(Info, State) ->
     {stop, {odd_info, Info}, State}.
 
